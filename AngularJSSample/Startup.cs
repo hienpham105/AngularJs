ade071bb3695fda7be41ecf560d746ffb731f789
D Duu <D Duu@DESKTOP-UV2IARP>
2017-02-16 10:25:09 +0700

    Add project files.

---
+++ AngularJSSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJSSample.Startup))]
+namespace AngularJSSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
